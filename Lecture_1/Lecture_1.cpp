69504c594e84dff485d78a59500f4867d4219216
Sarthak Agarwal <93201986+SarthakAgarwal21@users.noreply.github.com>
2021-12-21 21:04:07 +0530

    Update Lecture_1.cpp
    Some basic changes

--- Lecture_1/Lecture_1.cpp
+++ Lecture_1/Lecture_1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello World";                                //This line displays a message "Hello World" onto the screen 
-    cout<<"This is a C++ class.";                       //This line displays another message onto the screen
+    cout << "Hello World";                                //This line displays a message "Hello World" onto the screen 
+    cout << "This is a C++ class.";                       //This line displays another message onto the screen
 
     /* This is a comment that 
        covers two lines. */              //Can span multible lines using '/*'
@@ -23,7 +23,7 @@ int main()
     int a = 5;                                           //int is used to show that 'a' is an integer value
     int b = 6;
 
-    cout<<a + b;                                         //to use cout it is necessary to include iostream
+    cout << a + b;                                       //to use cout it is necessary to include iostream
 
     int x_yz$ = 5;                                       //we can use alphabets, _ and underscores as the name of the variables
 
@@ -33,38 +33,39 @@ int main()
     long y= 6;
     short temp = -32768;
 
-    cout<<endl<<temp;
-    cout<<endl<<sizeof(long);       //This commands helps to show the size of the integer on your system because size is system dependent
+    cout << endl << temp;
+    cout << endl << sizeof(long);                         //This commands helps to show the size of the integer on your system because size is system dependent
 
-    //cout<< a- b;
+
 
     int $xyz = 5;
 
     char var = 91;                    // code using ASCII 91 - [
 
-    cout<<endl<<var;
+    cout << endl << var;
 
     bool new_var = true;               //bool var type, will see the purpose of true and false in later lectures
 
-    cout<<endl<<new_var;
+    cout << endl << new_var;
 
-    extern int s;                       //delay reservation of memory(out of syllabus). declaration and definition not happening together. declaration is name a whereas definition is reservation of memory.
+    extern int s;                      //delay reservation of memory(out of syllabus). declaration and definition not happening together. declaration is name a whereas definition is reservation of memory.
     
     
         //end of lecture 3
     
     
         
-        cout<<'\n'<<sizeof(76542L);
+        cout << '\n' << sizeof(76542L);
 
-        cout<<'\n'<<"Hello World\n";           //in new line, single qoute
-        cout<<"This is a C++ class.";
+        cout << '\n' << "Hello World\n";           //in new line, single qoute
+        cout << "This is a C++ class.";
 
          // String Constants
-    cout<<"";                   //Empty String
-    cout<<"h";                  //Single length string
-    cout<<"Hello World\n";      //String followed by new line
-    cout<<"'Good' Morning";     //'Good' Morning
-    cout<<"\"Good\" Morning";   //"Good" Morning
+    
+    cout << "";                   //Empty String
+    cout << "h";                  //Single length string
+    cout << "Hello World\n";      //String followed by new line
+    cout << "'Good' Morning";     //'Good' Morning
+    cout << "\"Good\" Morning";   //"Good" Morning
         return 0;
 }
